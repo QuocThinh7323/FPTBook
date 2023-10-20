3b2fd3659cf874785280058ce9ff55e7bb1f7ba9
QuocThinh7323 <121931865+QuocThinh7323@users.noreply.github.com>
2023-10-20 18:13:56 +0700

    add TmpCategory and order models

---
+++ FPTBook/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FPTBook.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string? UserID { get; set; }
+        public ApplicationUser? User { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime OrderTime { get; set; }
+        public decimal Total { get; set; }
+        public int State { get; set; }
+        public ICollection<OrderItem>? OrderItem { get; set; }
+    }
+}
--- FPTBook/Models/TmpCategory.cs
+++ FPTBook/Models/TmpCategory.cs
@@ -2,7 +2,5 @@
 {
     public class TmpCategory
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
     }
 }
