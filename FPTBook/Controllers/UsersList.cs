bcd7a493abec9ec282cb4d6ff91905bfb6c880d4
QuocThinh7323 <121931865+QuocThinh7323@users.noreply.github.com>
2023-10-21 08:03:06 +0700

    add UsersListController Controller

--- FPTBook/Controllers/UsersList.cs
+++ FPTBook/Controllers/UsersList.cs
@@ -1,6 +1,62 @@
-﻿namespace FPTBook.Controllers
+﻿using FPTBook.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FPTBook.Controllers
 {
     public class UsersList
     {
+        [Authorize(Roles = "Admin")]
+        public class UsersListController : Controller
+        {
+            private readonly ApplicationDbContext _context;
+            private readonly UserManager<ApplicationUser> _userManager;
+            public UsersListController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+            {
+                _context = context;
+                _userManager = userManager;
+            }
+
+            //GET: UsersController
+            public async Task<IActionResult> Index()
+            {
+                return View(await _context.Users.ToListAsync());
+            }
+
+            //GET: SetPassword
+            public IActionResult SetPassword(string id)
+            {
+                ViewBag.UID = id;
+                return View();
+            }
+
+            //POST: UsersController/SetPassword
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> SetPassword([Bind("UID, NewPassword, ConfirmPassword")] SetPasswordViewModel pass)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View();
+                }
+                var user = await _userManager.FindByIdAsync(pass.UID);
+                if (user == null)
+                {
+                    return NotFound($"Unable to load user with ID'{pass.UID}'.");
+                }
+                await _userManager.RemovePasswordAsync(user);
+                var addPasswordResult = await _userManager.AddPasswordAsync(user, pass.NewPassword);
+                if (addPasswordResult.Succeeded)
+                {
+                    foreach (var error in addPasswordResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
--- FPTBook/Models/OrderItem.cs
+++ FPTBook/Models/OrderItem.cs
@@ -2,5 +2,11 @@
 {
     public class OrderItem
     {
+        public int Id { get; set; }
+        public int BookID { get; set; }
+        public Book? Book { get; set; }
+        public int Quantity { get; set; }
+        public int OrderID { get; set; }
+        public Order? Order { get; set; }
     }
 }
