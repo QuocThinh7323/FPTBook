dd67af6c19ff6b8bd5ca348bd8aef8e1fd111e09
TranNguyetCan <Cantngcc210146@fpt.edu.vn>
2023-10-22 15:24:56 +0700

    detail publishingcompanies

---
+++ FPTBook/Controllers/PublishingCompaniesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FPTBook.Controllers
+{
+    public class PublishingCompaniesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
