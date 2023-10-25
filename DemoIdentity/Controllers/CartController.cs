22694dbb372346e4220682b218cfc727ff802296
NguyenHaNhatLinh <121932006+NguyenHaNhatLinh@users.noreply.github.com>
2023-10-25 17:46:56 +0700

    Add to cart

---
+++ DemoIdentity/Controllers/CartController.cs
@@ -0,0 +1,80 @@
+﻿using FPTBook.Models;
+using FPTBookDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FPTBook.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ILogger<CartController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        // Key lưu chuỗi json của Cart
+        public const string CARTKEY = "cart";
+
+        public CartController(ILogger<CartController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+		// Hiện thị giỏ hàng
+		[Route("/cart", Name = "cart")]
+		public IActionResult Index()
+        {
+			return View(GetCartItems());
+		}
+
+        // Lấy cart từ Session (danh sách CartItem)
+        List<CartItem> GetCartItems()
+        {
+
+            var session = HttpContext.Session;
+            string jsoncart = session.GetString(CARTKEY);
+            if (jsoncart != null)
+            {
+                return JsonConvert.DeserializeObject<List<CartItem>>(jsoncart);
+            }
+            return new List<CartItem>();
+        }
+
+        // Lưu Cart (Danh sách CartItem) vào session
+        void SaveCartSession(List<CartItem> ls)
+        {
+            var session = HttpContext.Session;
+            string jsoncart = JsonConvert.SerializeObject(ls);
+            session.SetString(CARTKEY, jsoncart);
+        }
+
+        /// Thêm sản phẩm vào cart
+		[Route("addcart/{productid:int}", Name = "addcart")]
+        public IActionResult AddToCart([FromRoute] int productid)
+        {
+            var product = _db.Book
+                            .Where(p => p.Id == productid)
+                            .FirstOrDefault();
+            if (product == null)
+                return NotFound("Không có sản phẩm");
+
+            // Xử lý đưa vào Cart ...
+            var cart = GetCartItems();
+            var cartitem = cart.Find(p => p.Book.Id == productid);
+            if (cartitem != null)
+            {
+                // Đã tồn tại, tăng thêm 1
+                cartitem.Quantity++;
+            }
+            else
+            {
+                //  Thêm mới
+                cart.Add(new CartItem() { Quantity = 1, Book = product });
+            }
+
+            // Lưu cart vào Session
+            SaveCartSession(cart);
+            // Chuyển đến trang hiện thị Cart
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ DemoIdentity/Models/CartItem.cs
@@ -0,0 +1,8 @@
+﻿namespace FPTBook.Models
+{
+    public class CartItem
+    {
+        public int Quantity { set; get; }
+        public Book Book { set; get; }
+    }
+}
